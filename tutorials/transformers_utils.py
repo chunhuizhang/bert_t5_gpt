49ea6b468a34e5bd3e7d0e52e05794f243f9a51a
lanchunhui <zch921005@126.com>
2023-04-16 11:58:12 +0800

    fine-tune transformers

---
+++ tutorials/transformers_utils.py
@@ -0,0 +1,32 @@
+# import importlib
+# importlib.reload(py_file)
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
+
+# for classification 
+def plot_confusion_matrix(y_preds, y_true, labels):
+    cm = confusion_matrix(y_true, y_preds, normalize="true")
+    fig, ax = plt.subplots(figsize=(4, 4))
+    disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=labels)
+    disp.plot(cmap="Blues", values_format=".2f", ax=ax, colorbar=False)
+    plt.title("Normalized confusion matrix") 
+
+# trainable parameters of the model
+def get_params(model):
+    model_parameters = filter(lambda p: p.requires_grad, model.parameters())
+    params = sum([np.prod(p.size()) for p in model_parameters])
+    return params
+
+def compute_classification_metrics(pred):
+    # pred: PredictionOutput, from trainer.predict(dataset)
+    # true label
+    labels = pred.label_ids
+    # pred
+    preds = pred.predictions.argmax(-1)
+    f1 = f1_score(labels, preds, average="weighted")
+    acc = accuracy_score(labels, preds)
+    precision = precision_score(labels, preds, average='macro')
+    return {"accuracy": acc, "f1": f1, 'precision': precision}
